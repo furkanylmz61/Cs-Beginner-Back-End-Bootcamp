0a809980e7fffeb1276adbed7314b638e1bf02a4
Ömer Furkan Yılmaz <furkanylm30@gmail.com>
2025-01-06 10:39:11 +0300

    initial commit

---
+++ FormsApp/FormsApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FormsApp.Controllers;
+
+public class HomeController : Controller
+{
+
+    public HomeController()
+    {
+       
+    }
+
+    public IActionResult Index(string searchString, string category)
+    {
+        var products = Repository.Products;
+        
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            ViewBag.SearchString = searchString;
+            products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
+        }
+
+        if (!String.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(P => P.CategoryId == int.Parse(category)).ToList();
+        }
+
+        var model = new ProductViewModel
+        {
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
+        //ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "CategoryName",category);
+        return View(model);
+    }
+    
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "CategoryName");
+        return View();
+    }
+
+    public IActionResult Create(Product model)
+    {
+        if (ModelState.IsValid)
+        {
+            model.ProductId = Repository.Products.Count + 1;
+            Repository.CreateProduct(model);
+            return RedirectToAction("Index");
+        }
+
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "CategoryName");
+        return View(model);
+    }
+    
+
+   
+}
